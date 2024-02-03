4593524af48cfd15c33f2a74de3a6d4640da345d
Diog007 <diogo.mz123@gmail.com>
2024-02-03 20:12:40 -0300

    Project started

---
+++ src/test/java/screensync/api/ScreensyncApplicationTests.java
@@ -0,0 +1,13 @@
+package screensync.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreensyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
