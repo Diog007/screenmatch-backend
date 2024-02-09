84549e3b224e7995fcf7868e6f05f2936aece638
Diog007 <diogo.mz123@gmail.com>
2024-02-09 14:56:56 -0300

    Implementede list the bests epidode 5

--- src/main/java/screensync/api/principal/Principal.java
+++ src/main/java/screensync/api/principal/Principal.java
@@ -7,8 +7,10 @@ import screensync.api.service.ConsumoApi;
 import screensync.api.service.ConverteDados;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Principal {
 
@@ -35,8 +37,18 @@ public class Principal {
 			temporadas.add(dadosTemporada);
 		}
 		temporadas.forEach(System.out::println);
-        
 
         temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
+
+        List<DadosEpisodio> dadosEpisodios = temporadas.stream()
+                .flatMap(t -> t.episodios().stream())
+                .collect(Collectors.toList());
+
+        System.out.println("\nTop 5 episodios");
+        dadosEpisodios.stream()
+                .filter(e -> !e.avaliacao().equalsIgnoreCase("N/A"))
+                .sorted(Comparator.comparing(DadosEpisodio::avaliacao).reversed())
+                .limit(5)
+                .forEach(System.out::println);
     }
 }
