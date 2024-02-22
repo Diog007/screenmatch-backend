9e426649227dc0975020260b363a9f994a1080e7
Diog007 <diogo.mz123@gmail.com>
2024-02-22 16:13:29 -0300

    Method created to search for a series

--- src/main/java/screensync/api/controller/SerieController.java
+++ src/main/java/screensync/api/controller/SerieController.java
@@ -2,6 +2,7 @@ package screensync.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import screensync.api.dto.SerieDTO;
@@ -29,5 +30,10 @@ public class SerieController {
     @GetMapping("/lancamentos")
     public List<SerieDTO> obterLancamentos(){
         return service.obterLancamento();
+    }
+
+    @GetMapping("/{id}")
+    public SerieDTO obterPorId (@PathVariable Long id){
+        return service.obterPorId(id);
     }
 }
--- src/main/java/screensync/api/service/SerieService.java
+++ src/main/java/screensync/api/service/SerieService.java
@@ -6,7 +6,9 @@ import screensync.api.dto.SerieDTO;
 import screensync.api.model.Serie;
 import screensync.api.repository.SerieRepository;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -24,8 +26,14 @@ public class SerieService {
     public List<SerieDTO> obterLancamento() {
         return converteDados(repository.findTop5ByOrderByEpisodiosDataLancamentoDesc());
     }
-
-
+    public SerieDTO obterPorId(Long id) {
+        Optional<Serie> serie = repository.findById(id);
+        if(serie.isPresent()){
+            Serie s = serie.get();
+            return new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(), s.getAvaliacao(), s.getGenero(), s.getAtores(), s.getPoster(), s.getSinopse());
+        }
+        return null;
+    }
 
     private List<SerieDTO> converteDados(List<Serie> series){
         return series.stream()
